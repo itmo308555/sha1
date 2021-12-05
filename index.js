773d862d3dd56452f6886ec58e5c4df7bb1baade
baovn-2976 <74024040+baovn-2976@users.noreply.github.com>
2021-12-05 11:13:15 +0300

    new

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import multer from "multer";
+import sizeOf from "image-size";
+import sharp from "sharp";
+import fs from "fs";
+import axios from "axios";
+import crypto from "crypto";
+
+const app = express();
+
+const img = multer({
+  dest: "./img",
+});
+
+app
+  .use(express.urlencoded({ extended: true }))
+  .set("view engine", "ejs")
+  .set("views", "views")
+  .get("/", (r) => r.res.render("./index"))
+  
+  .get("/sha1", (r) => {
+    r.res.render("./sha", { value: "" });
+  })
+  .post("/sha1", (r) => {
+    r.res.render("./sha", {
+      value: crypto.createHash("sha1").update(r.body.inp).digest("hex"),
+    });
+  })
+  .all("/login", (r) => r.res.send("bao-vn"))
+  .listen(process.env.PORT || 3000, () => {
+    console.log("Server is working");
+  });
